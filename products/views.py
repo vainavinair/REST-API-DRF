0e50f426dd2b44239db21790294b381651be2571
VanV <vainavinair2009558@gmail.com>
2023-05-08 21:38:42 +0530

    added basic serailizer

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Products
 
-# Create your views here.
+@api_view(['GET'])
+def home(request):
+    data = Products.objects.all()
+    serializer = ProductSerializer(data, many=True)
+    return Response(serializer.data)
