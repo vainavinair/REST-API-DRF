24ff9c79925107ca6c27c95cc461dd8a2b24d00c
VanV <vainavinair2009558@gmail.com>
2023-05-08 21:21:45 +0530

    added product serailizers

---
+++ products/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Products
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+        fields = [
+            'id',
+            'name',
+            'description',
+            'price',
+            'image'
+        ]
