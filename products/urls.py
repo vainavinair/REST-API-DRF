7f55c8ddf8b831d5950c35e4105107eb5061e0b1
VanV <vainavinair2009558@gmail.com>
2023-05-10 16:07:38 +0530

    CRUD using CBV

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import ProductList, ProductCreate, ProductUpdate, ProductDelete
 
 urlpatterns = [
-    path('home/', home, name='api-home'),
+    path('home/', ProductList.as_view(), name='api-home'),
+    path('create/', ProductCreate.as_view(), name='api-create'),
+    path('<id>/update/', ProductUpdate.as_view(), name='api-update'),
+    path('<id>/delete/', ProductDelete.as_view(), name='api-delete'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,60 @@
-from django.shortcuts import render
-from .serializers import ProductSerializer
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .models import Products
+from django.shortcuts import render, get_object_or_404
 
-@api_view(['GET'])
-def home(request):
-    data = Products.objects.all()
-    serializer = ProductSerializer(data, many=True)
-    return Response(serializer.data)
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.decorators import api_view
+
+from .models import Products
+from .serializers import ProductSerializer
+
+
+# @api_view(['GET'])
+# def home(request):
+#     data = Products.objects.all()
+#     serializer = ProductSerializer(data, many=True)
+#     return Response(serializer.data)
+
+def validate_decription(serializer):
+    name = serializer.validated_data.get('name')
+    description = serializer.validated_data.get('description') or None
+    if description is None:
+        description = name
+    return description
+
+
+class ProductList(generics.ListAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductCreate(generics.CreateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        name = serializer.validated_data.get('name')
+        description = serializer.validated_data.get('description') or None
+        if description is None:
+            description = name
+        serializer.save(description=description)
+
+class ProductUpdate(generics.RetrieveUpdateAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
+
+    def perform_update(self, serializer):
+        description = validate_decription(serializer)
+        serializer.save(description=description)
+
+
+class ProductDelete(generics.DestroyAPIView):
+    queryset = Products.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        self.perform_destroy(instance)
+        return Response(serializer.data)
